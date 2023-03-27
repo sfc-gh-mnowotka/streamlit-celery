a353e7622f090190aac81072fce83305a51b7be4
sfc-gh-mnowotka <michal.nowotka@snowflake.com>
2023-03-27 10:45:56 +0200

    Stopping celery

--- app.py
+++ app.py
@@ -6,27 +6,40 @@ from celery_utils import start_celery_worker
 from celery_utils import start_celery_beat
 from celery_utils import is_celery_beat_working
 from celery_utils import are_workers_running
+from celery_utils import stop_celery
 
 
 st.title("🥬Using with Celery")
 
-if st.button("Start Celery Beat!", disabled=is_celery_beat_working()):
-    thread = threading.Thread(target=start_celery_beat, name="Celery beat control thread")
-    thread.daemon = True
-    thread.start()
-    with st.spinner("Starting Celery Beat..."):
-        time.sleep(1)
-    st.experimental_rerun()
-
-if st.button("Start Celery Workers!", disabled=are_workers_running()):
-    thread = threading.Thread(target=start_celery_worker, name="Celery worker control thread")
-    thread.daemon = True
-    thread.start()
-    with st.spinner("Starting Celery Workers..."):
-        time.sleep(1)
-    st.experimental_rerun()
-
 if st.button("Fetch results"):
     results = fetch_results()
     st.write(results)
+
+with st.expander("⚙️ Celery controls"):
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        is_beat_working = is_celery_beat_working()
+        label = "💓 Start Celery Beat!" if not is_beat_working else "💓 Celery Beat Started!"
+        if st.button(label, disabled=is_beat_working):
+            thread = threading.Thread(target=start_celery_beat, name="Celery beat control thread")
+            thread.daemon = True
+            thread.start()
+            with st.spinner("Starting Celery Beat..."):
+                time.sleep(1)
+            st.experimental_rerun()
+
+    with col2:
+        are_workers_started = are_workers_running()
+        label = "🔨Start Celery Workers!" if not are_workers_started else "🔨 Celery Workers Started!"
+        if st.button(label, disabled=are_workers_started):
+            thread = threading.Thread(target=start_celery_worker, name="Celery worker control thread")
+            thread.daemon = True
+            thread.start()
+            with st.spinner("Starting Celery Workers..."):
+                time.sleep(1)
+            st.experimental_rerun()
+
+    with col3:
+        if st.button("Stop celery"):
+            stop_celery()
 
--- celery_utils.py
+++ celery_utils.py
@@ -44,12 +44,7 @@ def start_celery_beat():
 
 def is_celery_beat_working():
     if CELERY_RUN_PROCESSES:
-        p1 = subprocess.Popen(["ps"], stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(["grep"] + [f"{CELERY_BEAT_COMMAND}"], stdin=p1.stdout, stdout=subprocess.PIPE)
-        p3 = subprocess.Popen(["wc", "-l"], stdin=p2.stdout, stdout=subprocess.PIPE)
-        p1.stdout.close()
-        p2.stdout.close()
-        return int(p3.communicate()[0].strip()) > 1
+        return _is_process_running(CELERY_BEAT_COMMAND)
     else:
         schedule_file = Path(CELERY_SCHEDULE_FILE)
         return schedule_file.is_file()
@@ -57,13 +52,48 @@ def is_celery_beat_working():
 
 def are_workers_running():
     if CELERY_RUN_PROCESSES:
-        p1 = subprocess.Popen(["ps"], stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(["grep"] + [f"{CELERY_WORKER_COMMAND}"], stdin=p1.stdout, stdout=subprocess.PIPE)
-        p3 = subprocess.Popen(["wc", "-l"], stdin=p2.stdout, stdout=subprocess.PIPE)
-        p1.stdout.close()
-        p2.stdout.close()
-        return int(p3.communicate()[0].strip()) > 1
+        return _is_process_running(CELERY_WORKER_COMMAND)
     else:
         broker_file = Path(CELERY_BROKER_URL.split(":///")[1])
         return broker_file.is_file()
 
+
+def stop_celery():
+    _stop_process(CELERY_WORKER_COMMAND)
+
+
+def _stop_process(command: str) -> bool:
+    print(f"Stopping process {command}")
+    grep_command = _prepare_command_for_grep(CELERY_WORKER_COMMAND)
+    p1 = subprocess.Popen(["ps"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["grep"] + [f"{grep_command}"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p3 = subprocess.Popen(["awk", "{ print $1 }"], stdin=p2.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()
+    p2.stdout.close()
+    print(f'ps | grep "{grep_command}" | awk \'{{ print $1 }}\'')
+    output = p3.communicate()[0]
+    pids = [str(pid) for pid in output.split()]
+
+    print(output.split())
+
+
+def _is_process_running(command: str) -> bool:
+    print(f"Checking if {command} is running")
+    grep_command = _prepare_command_for_grep(CELERY_WORKER_COMMAND)
+    p1 = subprocess.Popen(["ps"], stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["grep"] + [f"{grep_command}"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p3 = subprocess.Popen(["wc", "-l"], stdin=p2.stdout, stdout=subprocess.PIPE)
+    p1.stdout.close()
+    p2.stdout.close()
+    print(f'ps | grep "{grep_command}" | wc -l')
+    output = p3.communicate()[0]
+    return int(output.strip()) > 0
+
+
+def _prepare_command_for_grep(command: str):
+    """
+    ps ... | grep returns grep itself so use this hack
+    https://www.cyberciti.biz/tips/grepping-ps-output-without-getting-grep.html
+    """
+    first_letter, *rest = command
+    return f"[{first_letter}]{''.join(rest)}"
